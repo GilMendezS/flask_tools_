a61bc3c0071f517e219724cd5227c09d4a47458a
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2019-01-14 17:42:19 -0600

    :bookmark: controller modified

--- src/app/posts/controllers.py
+++ src/app/posts/controllers.py
@@ -1,13 +1,13 @@
 from app import app
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
 
 from app.posts.forms import PostForm
 from  app.posts.models import Post
 
-mod_post = Blueprint('posts',__name__, url_prefix='/posts')
+mod_post = Blueprint('posts',__name__)
 
 @mod_post.route('/')
 def post_index():
     posts = Post.query.all()
-    return render_template('posts/index.html', posts = posts)
+    return jsonify(posts)
